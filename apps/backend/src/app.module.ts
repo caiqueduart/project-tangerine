266c3f1e3d0ae12a5bc2f0bd531621f28baffb0a
Caique Duarte <dcaique242@gmail.com>
2026-07-27 16:48:39 -0300

    feat: usa variáveis de ambiente para banco de dados

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,15 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TownhouseModule } from './modules/townhouse/townhouse.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        AuthModule,
         TownhouseModule,
+        ConfigModule.forRoot(),
         TypeOrmModule.forRoot({
-            type: 'sqlite',
-            database: 'db.sqlite',
-            autoLoadEntities: true,
-            synchronize: true,
+            type: process.env.DATABASE_TYPE as 'postgres',
+            database: process.env.DATABASE_PATH,
+            autoLoadEntities: Boolean(process.env.DATABASE_AUTO_LOAD_ENTITIES),
+            synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
         }),
     ],
     controllers: [AppController],
